d6ef85bb2b64ffb1bd8b0abe4fdc3e4845b55f28
Isuru <isuru623@gmail.com>
2024-02-27 15:56:35 +0530

    Add script to switch traffic in azure traffic manager

---
+++ switch_to_east.sh
@@ -0,0 +1,2 @@
+az network traffic-manager endpoint update --name east-us --profile-name wso2-is --resource-group rnd-isurug-charindu --type azureEndpoints --endpoint-status Enabled
+az network traffic-manager endpoint update --name central-us --profile-name wso2-is --resource-group rnd-isurug-charindu --type azureEndpoints --endpoint-status Disabled
