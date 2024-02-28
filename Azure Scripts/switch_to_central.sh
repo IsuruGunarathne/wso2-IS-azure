14a114fb65c6de8a1f489f509895005818d9271b
Isuru <isuru623@gmail.com>
2024-02-28 10:15:31 +0530

    Add script to calculate time taken to switch

--- Azure Scripts/switch_to_central.sh
+++ Azure Scripts/switch_to_central.sh
@@ -6,6 +6,9 @@ trafficManagerProfile="wso2-is"
 endpointType="azureEndpoints"
 endpointResourceGroup="rnd-isurug-charindu"
 
+# Capture start time
+start_time=$(date +%s)
+
 echo "Initialting failover to central-us"
 az sql db replica set-primary --name $database --resource-group $DatabaseResourceGroup --server $Server
 echo "Database switched to central-us"
@@ -14,3 +17,10 @@ echo "Updating traffic manager endpoint status to central-us"
 az network traffic-manager endpoint update --name east-us --profile-name $trafficManagerProfile --resource-group $endpointResourceGroup --type $endpointType --endpoint-status Disabled
 az network traffic-manager endpoint update --name central-us --profile-name $trafficManagerProfile --resource-group $endpointResourceGroup --type $endpointType --endpoint-status Enabled
 echo "Traffic manager endpoint status updated"
+
+# Capture end time
+end_time=$(date +%s)
+
+# Calculate elapsed time
+elapsed_time=$((end_time - start_time))
+echo "Time taken: $elapsed_time seconds"
--- Azure Scripts/switch_to_east.sh
+++ Azure Scripts/switch_to_east.sh
@@ -6,6 +6,9 @@ trafficManagerProfile="wso2-is"
 endpointType="azureEndpoints"
 endpointResourceGroup="rnd-isurug-charindu"
 
+# Capture start time
+start_time=$(date +%s)
+
 echo "Initialting failover to east-us"
 az sql db replica set-primary --name $database --resource-group $DatabaseResourceGroup --server $Server
 echo "Database switched to east-us"
@@ -14,3 +17,10 @@ echo "Updating traffic manager endpoint status to east-us"
 az network traffic-manager endpoint update --name east-us --profile-name $trafficManagerProfile --resource-group $endpointResourceGroup --type $endpointType --endpoint-status Enabled
 az network traffic-manager endpoint update --name central-us --profile-name $trafficManagerProfile --resource-group $endpointResourceGroup --type $endpointType --endpoint-status Disabled
 echo "Traffic manager endpoint status updated"
+
+# Capture end time
+end_time=$(date +%s)
+
+# Calculate elapsed time
+elapsed_time=$((end_time - start_time))
+echo "Time taken: $elapsed_time seconds"
