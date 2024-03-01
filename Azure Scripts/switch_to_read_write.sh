caabbda65190916488292249dbe7ea98c28da669
Isuru <isuru623@gmail.com>
2024-03-01 11:06:05 +0530

    Add changed file name to helm upgrade

--- Azure Scripts/switch_to_read_write.sh
+++ Azure Scripts/switch_to_read_write.sh
@@ -12,7 +12,7 @@ echo " "
 echo " "
 
 echo "upgrading the IS deployment"
-helm upgrade is-release ../IS --force
+helm upgrade is-release -f is-configmap.yaml ../IS --force
 echo "IS deployment upgraded successfully."
 echo " "
 echo " "
