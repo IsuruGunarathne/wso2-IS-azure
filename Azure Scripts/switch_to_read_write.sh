3aa3e4c00a8d097646802eeab6686a3b86178043
Isuru <isuru623@gmail.com>
2024-03-01 11:21:54 +0530

    Change config name in deployment as well

--- Azure Scripts/switch_to_read_write.sh
+++ Azure Scripts/switch_to_read_write.sh
@@ -1,12 +1,14 @@
 # delete the is-configmap.yaml file in IS/templates folder
 
 rm -f ../IS/templates/is-configmap.yaml
+rm -f ../IS/templates/is-deployment.yaml
 echo "read-only is-configmap.yaml file deleted successfully."
 echo " "
 echo " "
 
 echo "replacing configmap with read-write configmap"
 cp configmaps/read_write/is-configmap.yaml ../IS/templates/
+cp configmaps/read_write/is-deployment.yaml ../IS/templates/
 echo "read-write is-configmap.yaml file copied successfully."
 echo " "
 echo " "
@@ -19,7 +21,9 @@ echo " "
 
 echo "replacing configmap with read-only is-configmap again"
 rm -f ../IS/templates/is-configmap.yaml
+rm -f ../IS/templates/is-deployment.yaml
 cp configmaps/read_only/is-configmap.yaml ../IS/templates/
+cp configmaps/read_only/is-deployment.yaml ../IS/templates/
 echo "read-only is-configmap.yaml file copied successfully."
 echo " "
 echo " "
