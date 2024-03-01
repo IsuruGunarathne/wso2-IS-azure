c3190856b8aab831823e8691ac80fb4a5f676c61
Isuru <isuru623@gmail.com>
2024-03-01 12:22:36 +0530

    Add instructions to the switch_to_read_write script

--- Azure Scripts/switch_to_read_write.sh
+++ Azure Scripts/switch_to_read_write.sh
@@ -1,4 +1,15 @@
-# delete the is-deployment.yaml file in IS/templates folder
+# what happens in this script?
+# 1. delete the is-deployment.yaml file in IS/templates folder
+# 2. copy the read-write is-deployment.yaml file to IS/templates folder
+# 3. upgrade the IS deployment
+# 4. delete the is-deployment.yaml file in IS/templates folder
+# 5. copy the read-only is-deployment.yaml file to IS/templates folder
+
+# if we want to switch to read-write mode, we can run this script, that's it
+
+# if we want to switch to read-only mode, we can run a helm upgrade command from the IS directory 
+# helm upgrade is-release ../IS
+
 
 rm -f ../IS/templates/is-deployment.yaml
 echo "read-only is-configmap.yaml file deleted successfully."
